9713b1d65bb1ed16e8b72b49582d9c3312390cb8
Caleb Hultmann <caleb.hultmann@gmail.com>
2024-12-13 15:47:59 -0500

    distance.py comments

--- ar_detection/detect_markers/distance.py
+++ ar_detection/detect_markers/distance.py
@@ -11,23 +11,46 @@ marker_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 param_markers = aruco.DetectorParameters()
 cap = cv2.VideoCapture(0)
 
+# direction determination function
+    # currently based on the x-axis return from the pose estimation function
+#
+# <--- -30 --- -10 --- 0 --- 10 --- 30 --->
+# far left   left   center   right   far right
+def determineDirection(x):
+    if (x < -30):
+        return "far left"
+    elif (x < -10):
+        return "left"
+    elif (x < 10):
+        return "center"
+    elif (x < 30):
+        return "right"
+    else:
+        return "far right"
+
 while True:
+    # grab frame and search for markers
     ret, frame = cap.read()
     if not ret:
         break  
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+    marker_corners, marker_IDs, _reject = aruco.detectMarkers(
         gray_frame, marker_dict, parameters=param_markers
     )
+
     if marker_corners:
         rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(
             marker_corners, MARKER_SIZE, mtx, dist
         )
         total_markers = range(0, marker_IDs.size)
+
+        # do work on each marker found
         for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
             cv2.polylines(
                 frame, [corners.astype(np.int32)], True, (0,255,255), 4, cv2.LINE_AA
             )
+
+            # refactor corners to be easier to work with
             corners = corners.reshape(4,2)
             corners = corners.astype(int)
             top_right = np.ravel(corners[0])
@@ -35,11 +58,14 @@ while True:
             bottom_right = np.ravel(corners[2])
             bottom_left = np.ravel(corners[3])
 
+            # calculate distance from camera to marker
             distance = np.sqrt(
                 tvecs[i][0][0]**2 + tvecs[i][0][1]**2 + tvecs[i][0][2]**2
             )
 
             point = cv2.drawFrameAxes(frame, mtx, dist, rvecs[i], tvecs[i], 4, 4)
+            
+            # add marker ID and distance to the fram
             cv2.putText(
                 frame,
                 f"id: {ids[0]} Dist: {distance:.2f} cm",
@@ -48,9 +74,8 @@ while True:
                 1.3,
                 (200,100,0),
                 2,
-                cv2.LINE_AA,
-            )
-
+                cv2.LINE_AA)
+            # add xyz coordinates to the frame
             cv2.putText(
                 frame,
                 f"x: {tvecs[i][0][0]:.2f} y: {tvecs[i][0][1]:.2f} z: {tvecs[i][0][2]:.2f}",
@@ -59,24 +84,18 @@ while True:
                 1.0,
                 (0,0,255),
                 2,
-                cv2.LINE_AA,
-            )
-            x = tvecs[i][0][0]
-            if (x < -30):
-                direction = "far left"
-            elif (x < -10):
-                direction = "left"
-            elif (x < 10):
-                direction = "center"
-            elif (x < 30):
-                direction = "right"
-            else:
-                direction = "far right"
+                cv2.LINE_AA)
+            
+            # print distance and direction for each marker
+            direction = determineDirection(tvecs[i][0][0])
             print(f"Marker ID: {i}   Distance: {distance:.2f}   Direction: {direction}"), 
 
+    # display frame and query for exit
     cv2.imshow("frame", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
+
+# clean up
 cap.release()
 cv2.destroyAllWindows()
